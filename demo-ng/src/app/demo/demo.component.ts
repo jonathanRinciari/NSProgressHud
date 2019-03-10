96ab0ed3046b3811fa326c8268cba52c25ae9f3f
Jonathan Rinciari <jrinciaritosd@gmail.com>
2019-03-10 19:48:14 -0400

    updated demo

---
+++ demo-ng/src/app/demo/demo.component.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit } from '@angular/core';
+import { NSProgressHud } from 'NSProgressHud';
+
+
+@Component({
+  moduleId: module.id,
+  selector: 'app-demo',
+  templateUrl: './demo.component.html',
+  styleUrls: ['./demo.component.scss']
+})
+export class DemoComponent implements OnInit {
+  private hud: NSProgressHud;
+  constructor() {}
+
+  ngOnInit() {
+    this.hud = new NSProgressHud();
+  }
+
+
+  show(type: string) {
+    if (type === 'indeterminate') {
+      this.hud.showProgress(null, {backgroundColor: '#EFEE00', progressType: 'indeterminate', size: {height: 200, width: 300}});
+
+      setTimeout(() => {
+        this.hud.dismiss();
+      }, 2000);
+    }
+
+    if (type === 'determinate') {
+      this.hud.showProgress(null, {hudColor: '#EFEE00', progressType: 'determinate', size: {height: 100, width: 100}});
+    }
+
+    if (type === 'bar') {
+      this.hud.showProgress(null, {hudColor: '#EFEE00', progressType: 'bar', size: {height: 100, width: 100}});
+    }
+
+    if (type === 'annular') {
+      this.hud.showProgress(null, {hudColor: '#EFEE00', progressType: 'annular', size: {height: 100, width: 100}});
+    }
+  }
+}

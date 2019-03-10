dc3b2a73d1109d59862c8cd1d1e116915717bbbd
Jonathan Rinciari <jrinciaritosd@gmail.com>
2019-03-10 19:48:50 -0400

    Updated demo

--- demo-ng/src/app/app-routing.module.ts
+++ demo-ng/src/app/app-routing.module.ts
@@ -1,14 +1,10 @@
 import { NgModule } from "@angular/core";
 import { NativeScriptRouterModule } from "nativescript-angular/router";
 import { Routes } from "@angular/router";
-
-import { ItemsComponent } from "./item/items.component";
-import { ItemDetailComponent } from "./item/item-detail.component";
+import { DemoComponent } from '~/app/demo/demo.component';
 
 const routes: Routes = [
-    { path: "", redirectTo: "/items", pathMatch: "full" },
-    { path: "items", component: ItemsComponent },
-    { path: "item/:id", component: ItemDetailComponent }
+    { path: "", component: DemoComponent }
 ];
 
 @NgModule({
